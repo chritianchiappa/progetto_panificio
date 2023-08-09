aae2dc262f321f84bd851d24ed7bbcc60a515828
chritianchiappa <c.chiappa66@gmail.com>
2023-08-09 16:59:00 +0200

    modifica chri

--- projectIS/crittografia/crittografa.py
+++ projectIS/crittografia/crittografa.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 import pickle
-#modifica michele 4.0
+#modifica michele 5.0
 class CryptoManager:
     def __init__(self):
         self.key = b'QaywJxlWb6nOrEt5X1zZawGstHK5sAWfUQpSejYhXIQ='
