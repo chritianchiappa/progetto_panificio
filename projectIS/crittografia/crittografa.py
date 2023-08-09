dfde4cc8f143feda1303624d5b87e1c8f6e62ac2
michelecarloni <carlonimichele3008@gmail.com>
2023-08-09 16:46:36 +0200

    modify

--- projectIS/crittografia/crittografa.py
+++ projectIS/crittografia/crittografa.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 import pickle
-#123
+#modifica michele 3.0
 class CryptoManager:
     def __init__(self):
         self.key = b'QaywJxlWb6nOrEt5X1zZawGstHK5sAWfUQpSejYhXIQ='
