30342dc0a60fc3e27db37b9f1df5fea23a515f10
chritianchiappa <c.chiappa66@gmail.com>
2023-08-11 14:13:38 +0200

    modifica aggiunta res_rc

--- projectIS/utilizzatore/view/VistaAmministratore.py
+++ projectIS/utilizzatore/view/VistaAmministratore.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget,QMessageBox
 from PyQt6 import uic
 from PyQt6.QtCore import QTime,QTimer
 from listadipendenti.view.vista_lista_dipendenti import VistaListaDipendenti
@@ -13,6 +13,7 @@ class VistaAmministratore(QWidget):
         self.magazzino_button.clicked.connect(self.go_Magazzino)
         self.statistiche_button.clicked.connect(self.go_Statistiche)
         self.cassa_button.clicked.connect(self.go_Cassa)
+        #self.logout_button.clicked.connect(self.go_Login)
         #self.logo_button.clicked.connect(webbrowser.open("https://www.tripadvisor.it/Restaurant_Review-g1934128-d15861838-Reviews-Il_Piccolo_Forno-Castelplanio_Province_of_Ancona_Marche.html"))
         self.timer=QTimer()
         self.timer.timeout.connect(self.update_time)
@@ -22,8 +23,10 @@ class VistaAmministratore(QWidget):
     def update_time(self):
         time=datetime.now()
         formatted_time= time.strftime("%H:%M:%S")
+        formatted_data=time.strftime("%d/%m/%Y")
         self.lcdOra.setDigitCount(8)
         self.lcdOra.display(formatted_time)
+        self.Data.setText(formatted_data)
 
     def go_Lista_Dipendenti(self):
         self.VistaDip = VistaListaDipendenti()
